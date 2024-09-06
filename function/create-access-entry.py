bb145d4994539b241b40473fc8c04d9751031641
jas0n1iu <gunszroses@gmail.com>
2024-09-06 10:21:53 +0800

    Update cf template

--- function/create-access-entry.py
+++ function/create-access-entry.py
@@ -15,28 +15,39 @@ def lambda_handler(event, context):
         cluster_names = eks_client.list_clusters()['clusters']
         for cluster_name in cluster_names:
             try:
-                # Create AWS authentication configuration map entry
-                eks_client.create_access_entry(
+                cluster_info = eks_client.describe_cluster(name=cluster_name)
+                cluster_version = cluster_info['cluster']['version']
+
+                if cluster_version >= '1.27':
+                    try:
+                        # Create AWS authentication configuration map entry
+                        eks_client.create_access_entry(
+                            clusterName=cluster_name,
+                            principalArn=lambda_role_arn,
+                            username='lambda-user',
+                            kubernetesGroups=['read-only-group']
+                        )
+                        print(f"AccessEntry created for cluster {cluster_name}")
+                    except eks_client.exceptions.ResourceInUseException:
+                        print(f"AccessEntry already exists for cluster {cluster_name}")
+                else:
+                    print(
+                        f"Skipping cluster {cluster_name} as it is using an older version ({cluster_version}) that does not support create_access_entry")
+                    continue
+
+                # Associate AmazonEKSViewPolicy with the Lambda role
+                eks_client.associate_access_policy(
                     clusterName=cluster_name,
                     principalArn=lambda_role_arn,
-                    username='lambda-user',
-                    kubernetesGroups=['read-only-group']
+                    policyArn=policy_arn,
+                    accessScope={
+                        'type': 'namespace',
+                        'namespaces': [namespace]
+                    }
                 )
-                print(f"AccessEntry created for cluster {cluster_name}")
-            except eks_client.exceptions.ResourceInUseException:
-                print(f"AccessEntry already exists for cluster {cluster_name}")
+            except eks_client.exceptions.ClientError as e:
+                print(f"Error processing cluster {cluster_name}: {e}")
                 continue
-
-            # Associate AmazonEKSViewPolicy with the Lambda role
-            eks_client.associate_access_policy(
-                clusterName=cluster_name,
-                principalArn=lambda_role_arn,
-                policyArn=policy_arn,
-                accessScope={
-                    'type': 'namespace',
-                    'namespaces': [namespace]
-                }
-            )
 
     return {
         'statusCode': 200,
